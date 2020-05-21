573f6febf52b0eb5f8d2f9b8ac03cf6d35b45850
Marvin Menchú <mmenchu@outlook.com>
2020-05-20 20:47:11 -0600

    usando appendTo y prependTo

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
   "use strict";
   $("main article:first").hide();
+  /*
   var articuloFinal = $("main article:last").clone();
   $("main").append(articuloFinal);
-  $("main").prepend(articuloFinal);
+  $("main").prepend(articuloFinal);*/
+
+  // usando appendTo y prependTo
+  var articuloFinal = $("main article:last").clone();
+  //$(articuloFinal).prependTo("main");
+  $(articuloFinal).appendTo("main");
 });
