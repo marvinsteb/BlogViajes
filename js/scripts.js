d37a7b448f454c849dc9b49433dc7160896bef07
Marvin Menchú <mmenchu@outlook.com>
2020-05-23 17:28:40 -0600

    agregando evento click en la imagen del logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
   "use strict";
+  /*
   $("main article:first").hide();
   /*
   var articuloFinal = $("main article:last").clone();
   $("main").append(articuloFinal);
-  $("main").prepend(articuloFinal);*/
+  $("main").prepend(articuloFinal);
 
   // usando appendTo y prependTo
   var articuloFinal = $("main article:last").clone();
   //$(articuloFinal).prependTo("main");
   $(articuloFinal).appendTo("main");
+  */
+  $("div.logo img").on("click", function () {
+    console.log("has echo click en el logo");
+  });
 });
