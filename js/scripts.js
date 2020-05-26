52df59a4fd5e3ee23c169e5bc88c6cc45c420dbc
Marvin Menchú <mmenchu@outlook.com>
2020-05-25 21:58:56 -0600

    cambiando el css con jquery

--- js/scripts.js
+++ js/scripts.js
@@ -63,15 +63,26 @@ $(document).ready(function () {
   */
   /*
   
-  $("#navegacion").show();
   $("main article:first h2").text("Cambiando el texto");
   // $("main article:first").next().html("<h1>nuevo titulo</h1>  ");
   $(".navegacion ul li:first a").attr("href", "https://outlook.com");
   $(".navegacion ul li:first a").attr("target", "_blanck");
   $("article:first img").attr("src", "img/imagen_2.jpg");
   */
-
+  /*
   $("main article:first img").on("click", function () {
     $(this).attr("src", "img/imagen_2.jpg");
+  });*/
+  $("#navegacion").show();
+  $(".logo img").css({ width: "600px" });
+  $("main article h2").css({ color: "#db008d" });
+  $("aside").css({
+    "background-color": "#e1e1e1",
+    "text-transform": "uppercase",
+    padding: "20px",
+  });
+
+  $(".navegacion ul li a ").on("mouseenter", function () {
+    $(".navegacion").css("background-color", "red");
   });
 });
