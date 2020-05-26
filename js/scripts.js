856b5a431794600812fe3441109cb6faef3dc2f6
Marvin Menchú <mmenchu@outlook.com>
2020-05-25 21:43:24 -0600

    cambiando imagen con attr()

--- js/scripts.js
+++ js/scripts.js
@@ -61,12 +61,17 @@ $(document).ready(function () {
     },
   });
   */
-
-  // recorriendo el DOM
+  /*
+  
   $("#navegacion").show();
   $("main article:first h2").text("Cambiando el texto");
   // $("main article:first").next().html("<h1>nuevo titulo</h1>  ");
   $(".navegacion ul li:first a").attr("href", "https://outlook.com");
   $(".navegacion ul li:first a").attr("target", "_blanck");
   $("article:first img").attr("src", "img/imagen_2.jpg");
+  */
+
+  $("main article:first img").on("click", function () {
+    $(this).attr("src", "img/imagen_2.jpg");
+  });
 });
