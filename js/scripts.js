89d97b806f33850a942312fe5c64a6b158e90d93
Marvin Menchú <mmenchu@outlook.com>
2020-05-31 14:21:26 -0600

    cambiando el evento click a mouseenter, agregando clase activo al div correspondiente al evento

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
   ("use strict");
   $(".nuestros-servicios div:first").show();
   $(".servicio nav a:first").addClass("activo");
-
-  $(".servicio nav a ").on("click", function () {
+  $(".servicio nav a ").on("mouseenter", function () {
+    $(".servicio nav a ").removeClass("activo");
     var enlace = $(this).attr("href");
-    $(".nuestros-servicios div").hide();
-    $(enlace).show();
+    $(".nuestros-servicios div").fadeOut();
+    $(enlace).fadeIn();
+    $(this).addClass("activo");
     return false;
   });
 });
