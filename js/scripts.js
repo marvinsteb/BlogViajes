724feda932c0dd12bc97f7b578a48d14f3da5986
Marvin Menchú <mmenchu@outlook.com>
2020-05-31 13:40:27 -0600

    agregando evento  para mostrar y ocultar los divs hijos de .nuestros-servicios

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
   ("use strict");
+  $(".nuestros-servicios div:first").show();
+  $(".servicio nav a:first").addClass("activo");
+
+  $(".servicio nav a ").on("click", function () {
+    var enlace = $(this).attr("href");
+    $(".nuestros-servicios div").hide();
+    $(enlace).show();
+    return false;
+  });
 });
